920a5771fa1af57dd5d45fd85f46da3a0b3313a3
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-01-24 14:08:25 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
 <footer>
-    <?= wp_footer(); ?>
+
+    <section>
+        <h3>Titre footer</h3>
+        <address>
+            <ul>
+                <li>Adresse 1</li>
+                <li>Adresse 2</li>
+                <li>Adresse 3</li>
+            </ul>
+        </address>
+    </section>
 </footer>
+<?= wp_footer(); ?>
 </body>
 
 </html>
