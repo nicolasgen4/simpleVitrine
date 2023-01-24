9f8e55e007cda3d9dfb4dbe5438eb3be85474dd1
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-01-24 14:07:41 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -2,8 +2,20 @@
 
 <main>
     <section>
-        <h2></h2>
-        <p></p>
+        <h2>Titre de section</h2>
+        <p>Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Accusamus fuga laborum vel
+            reiciendis quis inventore assumenda totam
+            non sint ut incidunt, eos consequatur perspiciatis
+            odit numquam harum magni facilis sed!</p>
+    </section>
+    <section>
+        <h2>Titre de section</h2>
+        <p>Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Accusamus fuga laborum vel
+            reiciendis quis inventore assumenda totam
+            non sint ut incidunt, eos consequatur perspiciatis
+            odit numquam harum magni facilis sed!</p>
     </section>
 </main>
 
