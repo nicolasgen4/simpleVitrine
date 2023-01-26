2af798ff7ae8a5d683db8b103ae6620b9a0a31b8
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-01-26 15:49:32 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,22 +1,23 @@
+<?php
+
+//Accès direct interdit
+if (!defined('ABSPATH')) {
+    http_response_code(403);
+    die('Erreur 403 : Forbidden');
+}
+
+?>
+
 <?= get_header(); ?>
 
-<main>
-    <section>
-        <h2>Titre de section</h2>
-        <p>Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Accusamus fuga laborum vel
-            reiciendis quis inventore assumenda totam
-            non sint ut incidunt, eos consequatur perspiciatis
-            odit numquam harum magni facilis sed!</p>
-    </section>
-    <section>
-        <h2>Titre de section</h2>
-        <p>Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Accusamus fuga laborum vel
-            reiciendis quis inventore assumenda totam
-            non sint ut incidunt, eos consequatur perspiciatis
-            odit numquam harum magni facilis sed!</p>
-    </section>
+<main id="accueil">
+        <h1><?= the_title(); ?></h1>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                        <?= the_content(); ?>
+
+        <?php endwhile;
+        endif ?>
 </main>
 
 <?= get_footer(); ?>
