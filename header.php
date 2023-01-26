84a4a359702ec91b27962f9261d2356c74a959ad
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-01-26 15:49:03 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<?php
+
+//Accès direct interdit
+if (!defined('ABSPATH')) {
+    http_response_code(403);
+    die('Erreur 403 : Forbidden');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -5,6 +15,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?= get_template_directory_uri() . '/assets/images/favicon.png' ?>" type="image/x-icon">
 
     <?= wp_head(); ?>
 
@@ -13,12 +24,18 @@
 <body>
 
     <header>
-        <h1>Titre principal</h1>
-        <nav>
-            <ul>
-                <li>Nav 1</li>
-                <li>Nav 2</li>
-                <li>Nav 3</li>
-            </ul>
-        </nav>
+        <div id="navigation-principale">
+            <?php if (function_exists('the_custom_logo')) : ?>
+                <?= the_custom_logo(); ?>
+            <?php endif ?>
+
+            <?= wp_nav_menu(
+                array(
+                    'menu' => 'primaire',
+                    'container' => 'nav',
+                    'theme_location' => 'primaire'
+
+                )
+            ) ?>
+        </div>
     </header>
